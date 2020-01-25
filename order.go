e77f089bd2aa5b9c6a1db9786e1e7baa454ecd18
Grégoire Delattre <gregoire.delattre@gmail.com>
2020-01-25 20:58:42 +0100

    Switch the scraper to use colly

---
+++ order.go
@@ -0,0 +1,30 @@
+package tpb
+
+import "fmt"
+
+// OrderBy represent the different values the search can be ordered by
+type OrderBy int
+
+// List of the different Orders available
+const (
+	OrderByName OrderBy = iota
+	OrderByDate
+	OrderBySize
+	OrderBySeeds
+	OrderByLeeches
+)
+
+// SortOrder represents the sort order
+type SortOrder int
+
+// List of the different sort order
+const (
+	Desc SortOrder = 1 + iota
+	Asc
+)
+
+// mapOrderBy takes the orderBy and sort order parameter and return the
+// corresponding option to pass to the website
+func mapOrderBy(orderBy OrderBy, order SortOrder) string {
+	return fmt.Sprintf("%d", int(orderBy)*2+int(order))
+}
